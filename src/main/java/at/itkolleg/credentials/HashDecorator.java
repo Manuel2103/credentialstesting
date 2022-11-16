a9efa1c8745631292c60ac084b2340e7a3182eca
ManuelFoidl <manufoidl@tsn.at>
2022-11-16 08:38:46 +0100

    Klasse HashDecorator Funktion export mit Hash und Test in Main

--- src/main/java/at/itkolleg/credentials/HashDecorator.java
+++ src/main/java/at/itkolleg/credentials/HashDecorator.java
@@ -1,5 +1,8 @@
 package at.itkolleg.credentials;
 
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class HashDecorator extends CsvExporter{
@@ -11,8 +14,12 @@ public class HashDecorator extends CsvExporter{
     @Override
     public void export(List<Credentials> credentialsList) {
         for (int i = 0; i < credentialsList.size(); i++) {
-           String hashPW =  credentialsList.get(i).getPwd();
-           credentialsList.get(i).setPwd("Test");
+
+            String hashPW = Hashing.sha256()
+                    .hashString(credentialsList.get(i).getPwd(), StandardCharsets.UTF_8)
+                    .toString();
+            credentialsList.get(i).setPwd(hashPW);
+
         }
         super.export(credentialsList);
     }
