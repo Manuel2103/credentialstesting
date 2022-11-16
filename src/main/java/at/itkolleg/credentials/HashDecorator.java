828960beb481ebb76536ee994dee0223ec3a3c9d
ManuelFoidl <manufoidl@tsn.at>
2022-11-16 08:34:54 +0100

    Klasse HashDecorator Funktion export mit Test Passwort

--- src/main/java/at/itkolleg/credentials/HashDecorator.java
+++ src/main/java/at/itkolleg/credentials/HashDecorator.java
@@ -1,6 +1,19 @@
 package at.itkolleg.credentials;
 
-public class HashDecorator{
+import java.util.List;
 
+public class HashDecorator extends CsvExporter{
 
+    public HashDecorator(CsvExporter csvExporter) {
+        super();
+    }
+
+    @Override
+    public void export(List<Credentials> credentialsList) {
+        for (int i = 0; i < credentialsList.size(); i++) {
+           String hashPW =  credentialsList.get(i).getPwd();
+           credentialsList.get(i).setPwd("Test");
+        }
+        super.export(credentialsList);
+    }
 }
--- src/main/java/at/itkolleg/credentials/Main.java
+++ src/main/java/at/itkolleg/credentials/Main.java
@@ -12,5 +12,9 @@ public class Main {
         credentialsList.add(new Credentials("www.xyz.net","9fas8dfu9ee", "bobba.fett"));
         ExportCredentials exportCredentials =  new CsvExporter();
         exportCredentials.export(credentialsList);
+
+        System.out.println("------------Decorator------------");
+        ExportCredentials exp = new HashDecorator(new CsvExporter());
+        exp.export(credentialsList);
     }
 }
